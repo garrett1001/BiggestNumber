e253de23163d6e7544110d660dec196f7ed09bda
garrett1001 <gwelton@mail.csuchico.edu>
2018-02-06 22:48:01 -0800

    completed

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,13 +5,29 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
+int first;
+int second;
+int biggest;
   
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+    cout<<"Please enter a whole number:\n";
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+    cin>>first;
+      
+    cout<<"Please enter another whole number:\n";
 
-  return 0;
+    cin>>second;
+
+    if(first>second)
+    {
+    biggest=first;
+    }
+    else
+    {
+    biggest=second;
+    }
+
+    cout<<"Of those two numbers, the biggest is: "<<biggest;
+    cout<<endl<<"Thank you for playing.\n";
+
+return 0;
 }
